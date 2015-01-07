ca180a970675806ff5e5d6d20d8c79d05f3f35f5
Grant Mills <grant.mills@bodybuilding.com>
2015-01-06 21:54:11 -0500

    adding delete functionality to FA and AG dashboard recent referrals

--- public/javascripts/src/directives/fresh-referral-directive.js
+++ public/javascripts/src/directives/fresh-referral-directive.js
@@ -11,6 +11,16 @@ app.directive('freshReferrals', ['$timeout', function(timer) {
 					searching: false,
 					info: false
 				});
+
+				$('#deleteModal').on('show.bs.modal', function (event) {
+					var button = $(event.relatedTarget) // Button that triggered the modal
+					var recipient = button.data('referral') // Extract info from data-*
+					var modal = $(this);
+					modal.find('.btn-primary').click(function () {
+						scope.deleteReferral(recipient);
+						modal.modal('hide');
+					});
+				});
 			}
 
 			FreshReferralController.init();
