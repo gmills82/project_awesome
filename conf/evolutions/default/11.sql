3a32db3e6559ee0ba5712dfdf9e4f12298e354d1
Grant Mills <grant.mills@bodybuilding.com>
2015-02-04 11:07:14 -0500

    more nav wording changes

--- conf/evolutions/default/11.sql
+++ conf/evolutions/default/11.sql
@@ -1,6 +1,8 @@
 # --- !Ups
 alter table action drop column image_name;
 INSERT into action (id, action_name, action_url, required_permission_level, short_description, category) values (9, 'Client Lookup', '/action/clients', 0, 'View Client Information', 'tool');
+UPDATE action set action_name='Create producer account' where id=3;
+UPDATE action set action_name='Create agent account' where id=4;
 
 # --- !Downs
 alter table action add column image_name set data type varchar(255);
