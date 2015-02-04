42db9b6baa4464e18d1fe450267b093cd86ee1df
Grant Mills <grant.mills@bodybuilding.com>
2015-02-04 11:02:11 -0500

    updated sql to use better wording on nav

--- conf/evolutions/default/11.sql
+++ conf/evolutions/default/11.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 alter table action drop column image_name;
-INSERT into action (id, action_name, action_url, required_permission_level, short_description, category) values (9, 'Manage Clients', '/action/clients', 0, 'Manage your clients', 'tool');
+INSERT into action (id, action_name, action_url, required_permission_level, short_description, category) values (9, 'Client Lookup', '/action/clients', 0, 'View Client Information', 'tool');
 
 # --- !Downs
 alter table action add column image_name set data type varchar(255);
