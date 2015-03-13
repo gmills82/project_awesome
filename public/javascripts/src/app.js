291a6cdfb2a8e6c4a70bb3311721d22349de9e9c
Grant Mills <grant.mills@bodybuilding.com>
2015-03-13 10:13:11 -0400

    almost have full 1:1 functionality and styling on fresh referrals

--- public/javascripts/src/app.js
+++ public/javascripts/src/app.js
@@ -1,5 +1,5 @@
 //Base app setup
-var app = angular.module("project_awesome", ["ngRoute", "validation.match", "ng-table"]);
+var app = angular.module("project_awesome", ["ngRoute", "validation.match", "ngTable"]);
 app.data = {};
 if(document.getElementById("baseContainer")){
 	app.data.currentUserId = document.getElementById("baseContainer").getAttribute("data-user");
