8bfde2f92287894bccb7afc7b5e693e6d4579a0a
Grant Mills <grant.mills@bodybuilding.com>
2015-03-17 22:30:13 -0400

    team referrals done

--- public/javascripts/src/controllers/view-team-referrals-controller.js
+++ public/javascripts/src/controllers/view-team-referrals-controller.js
@@ -1,9 +1,60 @@
 //Referrals By Creator Controller
-app.controller('ViewTeamReferralsController', ["$scope", "$http", function ($scope, $http){
+app.controller('ViewTeamReferralsController', ["$scope", "$http", "ngTableParams", "$filter", function ($scope, $http, ngTableParams, $filter){
 	$scope.referrals = [];
+	$scope.teamRefs = [{'title': 'No Filter', 'id': ''}];
+	$scope.arr=[];
+
+	//Helper function
+	var inArray = Array.prototype.indexOf ?
+		function (val, arr) {
+			return arr.indexOf(val)
+		} :
+		function (val, arr) {
+			var i = arr.length;
+			while (i--) {
+				if (arr[i] === val) return i;
+			}
+			return -1;
+		};
+
 	this.init = function () {
-		$http({"method": "GET", "url": "/json/referrals/team/" + app.data.currentUserId}).success(function (data){
-			$scope.referrals = data.data;
+		$scope.viewTeamReferrals = new ngTableParams({
+			page: 1,            // show first page
+			count: 10,
+			sorting: {
+				lastEditedDate: 'desc'
+			}
+		}, {
+			getData: function($defer, params) {
+				$http({"method": "GET", "url": "/json/referrals/team/" + app.data.currentUserId}).success(function (data){
+					//Filter
+					var filteredData = params.filter() ?
+						$filter('filter')(data.data, params.filter()) :
+						data.data;
+					//Then sort
+					var orderedData = params.sorting() ?
+						$filter('orderBy')(filteredData, params.orderBy()) :
+						filteredData;
+
+					//Pass out total to larger scope
+					$scope.total = orderedData.length;
+					params.total(orderedData.length);
+
+					//Create scope for RefType filter
+					angular.forEach(orderedData, function(item){
+						if (inArray(item.refType, $scope.arr) === -1) {
+							$scope.arr.push(item.refType);
+							$scope.teamRefs.push({
+								'id': item.refType,
+								'title': item.refType
+							});
+						}
+					});
+
+					//Resolve data gathering
+					$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+				});
+			}
 		});
 	}
 	$scope.deleteReferral = function (refId) {
