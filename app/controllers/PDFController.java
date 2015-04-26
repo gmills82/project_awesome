f650ab6f2f53147d9ce3725fd9688e9824295c36
Grant Mills <grant.mills@bodybuilding.com>
2015-04-26 02:06:56 -0400

    displaying blank new account pdf

--- app/controllers/PDFController.java
+++ app/controllers/PDFController.java
@@ -1,7 +1,14 @@
 package controllers;
 
+import org.apache.pdfbox.PDFReader;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.io.*;
+
 /**
  * User: grant.mills
  * Date: 4/24/15
@@ -9,6 +16,25 @@ import play.mvc.Result;
  */
 public class PDFController extends Controller {
 	public static Result newAccount(Long clientId) {
-		return redirect(routes.Application.login());
+		try {
+			InputStream fileStream = new FileInputStream("pdf/new-account.pdf");
+			PDFParser parser = new PDFParser(fileStream);
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			parser.parse();
+			PDDocument document = parser.getPDDocument();
+			document.save(output);
+			document.close();
+			response().setContentType("application/pdf");
+			return ok(output.toByteArray());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return internalServerError();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return internalServerError();
+		} catch (COSVisitorException e) {
+			e.printStackTrace();
+			return internalServerError();
+		}
 	}
 }
