458c94f9b075ba705c621519d4e7d79172fd2942
Grant Mills <grant.mills@bodybuilding.com>
2015-05-27 10:57:08 -0400

    optional params for passing in date to daily report

--- app/controllers/Reports.java
+++ app/controllers/Reports.java
@@ -4,6 +4,7 @@ import models.Referral;
 import models.UserModel;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Results;
 import utils.DateUtilities;
 import views.html.reports.dailyReferrals;
 
@@ -26,51 +27,61 @@ public class Reports extends Controller {
 	 * Daily referral page
 	 * @return
 	 */
-	public static Result dailyReferrals() {
 
+	public static Result dailyReferrals(Long date) {
 		UserModel currentUser = Application.getCurrentUser();
 		if(null != currentUser) {
 			Calendar todaysDate = new GregorianCalendar();
+			//If we set date to 0 it means use todays date
+			if(date != 0) {
+				Date chosenDate = new Date(date);
+				todaysDate.setTime(chosenDate);
+			}
 			todaysDate.set(Calendar.HOUR_OF_DAY, 0);
 			todaysDate.set(Calendar.MINUTE, 0);
 			todaysDate.set(Calendar.SECOND, 0);
 			todaysDate.set(Calendar.MILLISECOND, 0);
 
 			String formatedDateString = DateUtilities.getDateFormat().format(todaysDate.getTime());
-			List<Map<String, String>> referralList = new ArrayList<Map<String, String>>();
-
-			//Get all referrals in a given time
-			List<Referral> todaysReferrals = Referral.getByDate(todaysDate.getTime());
-			//Limit to your team's Referrals
-			Set<UserModel> team = UserModel.getChildUserModelsByParentAllLevels(currentUser);
-			team.add(currentUser);
-
-			List<Long> teamMemberIds = new ArrayList<Long>();
-			Iterator<UserModel> setIter = team.iterator();
-			while(setIter.hasNext()) {
-				UserModel teamMember = setIter.next();
-				teamMemberIds.add(teamMember.id);
-			}
-
-			//Filter
-			todaysReferrals = filter(having(on(Referral.class).creatorId, isIn(teamMemberIds)), todaysReferrals);
-
-			//TODO: Hitting database for each referral to get user name.
-			//TODO: Make method to get all based on a group of user ids
-			Iterator<Referral> iter = todaysReferrals.iterator();
-			while(iter.hasNext()) {
-				Referral ref = iter.next();
-				Map propMap = new HashMap<String, String>();
-				UserModel creator = UserModel.getById(ref.creatorId);
-
-				propMap.put(CLIENT_NAME, ref.clientName);
-				propMap.put(CREATOR_NAME, creator.firstName + " " + creator.lastName);
-
-				referralList.add(propMap);
-			}
+			List<Map<String, String>> referralList = getDailyReferrals(todaysDate, currentUser);
 
 			return ok(dailyReferrals.render(currentUser, formatedDateString, referralList));
 		}
 		return redirect(routes.Application.login());
+	}
+
+	private static List<Map<String, String>> getDailyReferrals(Calendar chosenDate, UserModel currentUser) {
+		//Get all referrals in a given time
+		List<Referral> todaysReferrals = Referral.getByDate(chosenDate.getTime());
+
+		//Limit to your team's Referrals
+		Set<UserModel> team = UserModel.getChildUserModelsByParentAllLevels(currentUser);
+		team.add(currentUser);
+
+		List<Long> teamMemberIds = new ArrayList<Long>();
+		Iterator<UserModel> setIter = team.iterator();
+		while(setIter.hasNext()) {
+			UserModel teamMember = setIter.next();
+			teamMemberIds.add(teamMember.id);
+		}
+
+		//Filter
+		todaysReferrals = filter(having(on(Referral.class).creatorId, isIn(teamMemberIds)), todaysReferrals);
+
+		//TODO: Hitting database for each referral to get user name.
+		//TODO: Make method to get all based on a group of user ids
+		List<Map<String, String>> referralList = new ArrayList<Map<String, String>>();
+		Iterator<Referral> iter = todaysReferrals.iterator();
+		while(iter.hasNext()) {
+			Referral ref = iter.next();
+			Map propMap = new HashMap<String, String>();
+			UserModel creator = UserModel.getById(ref.creatorId);
+
+			propMap.put(CLIENT_NAME, ref.clientName);
+			propMap.put(CREATOR_NAME, creator.firstName + " " + creator.lastName);
+
+			referralList.add(propMap);
+		}
+		return referralList;
 	}
 }
