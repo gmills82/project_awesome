6e4d988b75b9a1bf827281e04799f048a5a519e4
Grant Mills <grant.mills@bodybuilding.com>
2015-06-08 17:49:43 -0400

    was hanging because of VPN not code. emails are sendable

--- app/utils/MailUtilities.java
+++ app/utils/MailUtilities.java
@@ -12,16 +12,20 @@ import javax.mail.internet.*;
 public class MailUtilities {
 	private static Properties fMailServerConfig = new Properties();
 
+
 	static {
 		fMailServerConfig.setProperty("mail.debug", "true");
-		fMailServerConfig.setProperty("mail.smtp.host", "localhost");
+		fMailServerConfig.setProperty("mail.smtp.host", "smtp.1and1.com");
+		fMailServerConfig.setProperty("mail.smtp.port", "587");
+		fMailServerConfig.setProperty("mail.smtp.starttls.enable", "true");
+		fMailServerConfig.setProperty("mail.smtp.auth", "true");
 	}
 
 	/**
 	 * Send a single email.
 	 */
-	public void sendEmail(String aFromEmailAddr, String aToEmailAddr, String aSubject, String aBody){
-		Session session = Session.getDefaultInstance(fMailServerConfig,	null);
+	public void sendEmail(String aFromEmailAddr, String aToEmailAddr, String aSubject, String aBody) {
+		Session session = Session.getDefaultInstance(fMailServerConfig, new SMTPAuthenticator());
 		MimeMessage message = new MimeMessage(session);
 		try {
 			//the "from" address may be set in code, or set in the
@@ -33,9 +37,17 @@ public class MailUtilities {
 			message.setFrom(new InternetAddress(aFromEmailAddr));
 			message.setText(aBody);
 			Transport.send(message);
-		}
-		catch (MessagingException ex){
+		} catch (MessagingException ex) {
 			System.err.println("Cannot send email. " + ex);
+		}
+	}
+
+	private class SMTPAuthenticator extends javax.mail.Authenticator {
+		private String username = "admin@efsmanager.com";
+		private String password = "Pheonix0";
+
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password);
 		}
 	}
 }
