8900691ad73782a5bf25091dfbfe16ec7cd34e5e
Grant Mills <grant.mills@bodybuilding.com>
2015-06-24 17:46:14 -0400

    address on appt widget

--- public/javascripts/src/controllers/upcoming-appts-controller.js
+++ public/javascripts/src/controllers/upcoming-appts-controller.js
@@ -1,7 +1,7 @@
 //Fresh Referral Controller
 app.controller('UpcomingApptsController', ["$scope", "$http", "ngTableParams", "$filter", function ($scope, $http, ngTableParams, $filter){
 	$scope.referrals = [];
-	
+
 	this.init = function () {
 		$scope.upcomingApptsTable = new ngTableParams({
 			page: 1,            // show first page
@@ -20,6 +20,26 @@ app.controller('UpcomingApptsController', ["$scope", "$http", "ngTableParams", "
 					var orderedData = params.sorting() ?
 						$filter('orderBy')(filteredData, params.orderBy()) :
 						filteredData;
+
+					//Loop over appts and prepare combined address
+					angular.forEach(orderedData, function (value, key){
+						var address = "";
+						if(value.client.address1) {
+							address = value.client.address1;
+							if(value.client.city) {
+								address = address + ", " + value.client.city;
+							}
+							if(value.client.state) {
+								address = address + ", " + value.client.state;
+							}
+							if(value.client.zipcode) {
+								address = address + ", " + value.client.zipcode;
+							}
+
+						}
+
+						value.client.address = address;
+					});
 
 					//Pass out total to larger scope
 					$scope.upcomingApptsTotal = orderedData.length;
