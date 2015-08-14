4199e1f278100d8761299b74116e7a0e62f6babc
Grant Mills <grant.mills@bodybuilding.com>
2015-08-14 15:26:28 -0400

    modifying sql

--- conf/evolutions/default/15.sql
+++ conf/evolutions/default/15.sql
@@ -1,14 +1,5 @@
-# --- !Downs
+# --- !Ups
 ALTER TABLE referral ADD COLUMN "next_step_timestamp" TIMESTAMP WITHOUT TIME ZONE;
 
 # --- !Downs
-DELETE FROM client
-WHERE  id IN (SELECT client.id
-    FROM   client
-        LEFT JOIN referral
-            ON referral.client_id = client.id
-        WHERE  referral.client_id IS NULL
-            AND client.NAME IN (SELECT client.NAME
-                FROM   client
-                GROUP  BY client.NAME
-                HAVING Count(*) > 1))
+ALTER TABLE referral DROP COLUMN "next_step_timestamp";
