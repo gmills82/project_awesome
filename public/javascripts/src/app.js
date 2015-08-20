7e6c20b7201a4ecf417830285f36538187d2e3ab
Justin Podzimek <justin.podzimek@jpodz.local>
2015-08-19 20:52:28 -0600

    The date filter was throwing a silent error when the script was minified. Simplified it to remove the error.

--- public/javascripts/src/app.js
+++ public/javascripts/src/app.js
@@ -6,12 +6,16 @@ app.data = {};
  Date format filter for Angular templates. Allows us to keep a consistent date format across
  the entire application.
  */
-app.filter('formatDate', function ($filter) {
-    var dateFilter = $filter('date'),
-        dateFormat = "yyyy-MM-dd h:mm a";
-	return function (input) {
-        return dateFilter(new Date(input), dateFormat);
-	}
+app.filter('formatPrettyDate', function () {
+    return function (item) {
+        return moment(item).format("YYYY-MM-DD h:mm a");
+    };
+    //var dateFormat = "yyyy-MM-dd h:mm a";
+    //
+	//return function (input) {
+     //   return input.toUpperCase();
+     //   //return moment(input).format(dateFormat);
+	//}
 });
 if(document.getElementById("baseContainer")){
 	app.data.currentUserId = document.getElementById("baseContainer").getAttribute("data-user");
