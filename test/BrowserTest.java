99e4df661709d16e3cb9e1a0d20a9be31d80d33a
Grant Mills <grant.mills@bodybuilding.com>
2015-10-27 14:13:51 -0400

    Browser tests working with java 8 lambdas now. also in intellij.

--- test/BrowserTest.java
+++ test/BrowserTest.java
@@ -10,42 +10,38 @@ import static org.fest.assertions.Assertions.assertThat;
 public class BrowserTest {
 	@Test
 	public void loginSuccess() {
-		running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-			public void invoke(TestBrowser browser) {
-				browser.goTo("http://localhost:3333");
+		running(testServer(3333), HTMLUNIT, browser -> {
+			browser.goTo("http://localhost:3333");
 
-				//Fill in creds
-				browser.$("input[name=userName]").text("test@efsmanager.com");
-				browser.$("input[name=password]").text("test");
+			//Fill in creds
+			browser.$("input[name=userName]").text("test@efsmanager.com");
+			browser.$("input[name=password]").text("test");
 
-				//Submit by clicking the Login button
-				FluentList<FluentWebElement> submitButton = browser.$("input[type='submit']");
-				submitButton.click();
+			//Submit by clicking the Login button
+			FluentList<FluentWebElement> submitButton = browser.$("input[type='submit']");
+			submitButton.click();
 
-				//Assert good login directs to home page
-				assertThat(browser.url()).isEqualTo("http://localhost:3333/home");
-			}
+			//Assert good login directs to home page
+			assertThat(browser.url()).isEqualTo("http://localhost:3333/home");
 		});
 	}
 
 	@Test
 	public void login() {
-		running(testServer(3333), HTMLUNIT, new F.Callback<TestBrowser>() {
-			public void invoke(TestBrowser browser) {
-				browser.goTo("http://localhost:3333");
+		running(testServer(3333), HTMLUNIT, browser -> {
+			browser.goTo("http://localhost:3333");
 
-				//Fill in creds
-				browser.$("input[name=userName]").text("test@efsmanager.com");
-				browser.$("input[name=password]").text("saltyDog");
+			//Fill in creds
+			browser.$("input[name=userName]").text("test@efsmanager.com");
+			browser.$("input[name=password]").text("saltyDog");
 
-				//Submit by clicking the Login button
-				FluentList<FluentWebElement> submitButton = browser.$("input[type='submit']");
-				submitButton.click();
+			//Submit by clicking the Login button
+			FluentList<FluentWebElement> submitButton = browser.$("input[type='submit']");
+			submitButton.click();
 
-				//Assert good login directs to home page
-				assertThat(browser.url()).isEqualTo("http://localhost:3333/login");
-				assertThat(browser.$(".alert.alert-danger")).isNotEmpty();
-			}
+			//Assert good login directs to home page
+			assertThat(browser.url()).isEqualTo("http://localhost:3333/login");
+			assertThat(browser.$(".alert.alert-danger")).isNotEmpty();
 		});
 	}
 }
