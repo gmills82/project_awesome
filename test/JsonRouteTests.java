b5255dc848e9c523ac3f1021844476e1027b91bd
Grant Mills <grant.mills@bodybuilding.com>
2015-10-27 13:50:33 -0400

    cleaned up json routes tests and added test for excel endpoint

--- test/JsonRouteTests.java
+++ test/JsonRouteTests.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import play.Logger;
@@ -8,41 +9,30 @@ import play.libs.ws.WSResponse;
 import play.test.*;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import static org.junit.Assert.*;
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.assertThat;
 
+/**
+ * Testing json routes
+ */
 public class JsonRouteTests {
-
-	/**
-	 * Useful for testing http routes and live page status
-	 */
 
 	private WSResponse getUrl(String path) {
 		return WS.url("http://localhost:3333" + path).get().get(Long.valueOf("5000"));
 	}
 
-	private Object getJsonData(String path) {
-		Map<String, Object> dataMap = Collections.EMPTY_MAP;
-		ObjectMapper mapper = new ObjectMapper();
-		Object data = null;
-		try {
-			dataMap = mapper.readValue(getUrl(path).getBody(), HashMap.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		if(dataMap != Collections.EMPTY_MAP) {
-			data = dataMap.get("data");
-		}
-
-		return data;
+	private JsonNode getJsonData(String path) {
+		JsonNode node = getUrl(path).asJson();
+		return node.get("data");
 	}
 
+	/**
+	 * Tests for 200 OK Status on endpoint
+	 */
 	@Test
 	public void testJsonRouteStatus() {
 		running(testServer(3333), new Runnable() {
@@ -76,12 +66,57 @@ public class JsonRouteTests {
 		});
 	}
 
+	/**
+	 * Tests for a non null jsonNode in the data part of the response body
+	 */
 	@Test
 	public void testJsonRouteBody() {
 		running(testServer(3333), new Runnable() {
 			public void run() {
 				//Test for non empty body
 				assertNotNull("GET /json/assetTypes", getJsonData("/json/assetTypes"));
+				//ID 10 is test@efsmanager.com
+				assertNotNull("GET /json/user/:userId", getJsonData("/json/user/10"));
+				//ID 5324 is Frank Sinatra client
+				assertNotNull("GET /json/client/:clientId", getJsonData("/json/client/5324"));
+				assertNotNull("GET /json/client/history/:clientId", getJsonData("/json/client/history/5324"));
+				assertNotNull("GET /json/client/query/:query", getJsonData("/json/client/query/test"));
+				//ID 5184 is Frank Sinatra referral
+				assertNotNull("GET /json/referral/:referralId", getJsonData("/json/referral/5184"));
+				assertNotNull("GET /json/referrals/:userId", getJsonData("/json/referrals/10"));
+				assertNotNull("GET /json/referrals/upcoming/:userId", getJsonData("/json/referrals/upcoming/10"));
+				assertNotNull("GET /json/appts/upcoming/:userId", getJsonData("/json/appts/upcoming/10"));
+				assertNotNull("GET /json/referrals/creator/:userId", getJsonData("/json/referrals/creator/10"));
+				assertNotNull("GET /json/referrals/team/:userId", getJsonData("/json/referrals/team/10"));
+				assertNotNull("GET /json/referrals/processing/:userId", getJsonData("/json/referrals/processing/10"));
+				assertNotNull("GET /json/assetTypes", getJsonData("/json/assetTypes"));
+				assertNotNull("GET /json/debtTypes", getJsonData("/json/debtTypes"));
+				//ID 401 is testagent@efsmanager.com
+				assertNotNull("GET /agent/:agentId/team/referrals", getJsonData("/agent/401/team/referrals"));
+				assertNotNull("GET /stats/efs/:efsid/:from/:to", getJsonData("/stats/efs/10/1420494290844/1445873007394"));
+				assertNotNull("GET /stats/agent/:agentid/:from/:to", getJsonData("/stats/agent/401/1420494290844/1445873007394"));
+				//ID 402 is testlsp@efsmanager.com
+				assertNotNull("GET /stats/producer/:producerid/:from/:to", getJsonData("/stats/producer/402/1420494290844/1445873007394"));
+
+			}
+		});
+	}
+
+	/**
+	 * Tests that an excel spreadsheet download is being returned
+	 */
+	@Test
+	public void testJsonExcelRoute() {
+		running(testServer(3333), new Runnable() {
+			public void run() {
+				WSResponse response = getUrl("/agent/401/referrals");
+				Map<String, List<String>> headers = response.getAllHeaders();
+				List<String> contentTypeStringArray = new ArrayList<String>();
+				contentTypeStringArray.add("application/vnd.ms-excel");
+
+				assertTrue(headers.containsKey("Content-Type"));
+				assertEquals(contentTypeStringArray, headers.get("Content-Type"));
+
 			}
 		});
 	}
