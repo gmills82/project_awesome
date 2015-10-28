dc6ae38140f18f289f969743305d39573320ee89
Grant Mills <grant.mills@bodybuilding.com>
2015-10-27 20:39:07 -0400

    reverting 13.sql as it was also causing data issues

--- conf/evolutions/default/13.sql
+++ conf/evolutions/default/13.sql
@@ -2,4 +2,4 @@
 INSERT into action (id, action_name, action_url, required_permission_level, short_description, category) values (15, 'Daily Referrals Report', '/reports/daily-referrals', 0, 'Report of all team referrals created that day', 'tracking');
 
 # --- !Downs
-delete from action where id=15;
+delete action where id=15;
